b36913395ea63a382fc7b87bf0007949c3480f3f
gabi <144284968+ninikx@users.noreply.github.com>
2023-11-02 21:51:20 +0100

    removed assignment operator

--- hecate_lexer/src/token.rs
+++ hecate_lexer/src/token.rs
@@ -1,23 +1,10 @@
 use hecate_util::ast::BinaryOp;
 use hecate_util::ast::UnaryOp;
 
-// import all enum items to avoid typing and redundancy
-use hecate_util::ast::BinaryOp::*;
-use hecate_util::ast::UnaryOp::*;
-use crate::token::AssignmentOp::*;
-
-enum AssignmentOp {
-    Assign,
-    Add,
-    Sub,
-    Mul,
-    Div,
-}
-
 pub enum Token {
     Identifier,
     BinaryOp(BinaryOp),
     UnaryOp(UnaryOp),
-    AssignmentOp(AssignmentOp),
+    AssignmentOp,
     Delimiter
 }
