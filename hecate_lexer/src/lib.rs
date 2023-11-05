9bd4e0769cedf64bd36ed3b78141c76850bf94be
gabi <144284968+ninikx@users.noreply.github.com>
2023-11-05 18:26:35 +0100

    lexer is able to tokenize identifier

--- hecate_lexer/src/lib.rs
+++ hecate_lexer/src/lib.rs
@@ -28,35 +28,76 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Spanned<'a, Token>;
     fn next(&mut self) -> Option<Self::Item> {
         // skip all white spaces
-        while self.iter.peek().unwrap().is_whitespace() {
-            self.iter.next();
-            self.current_pos += 1;
+        loop {
+            match self.iter.peek() {
+                None => {
+                    return Option::Some(Spanned {
+                        inner: Token::EOF,
+                        loc: Span::Generated,
+                    });
+                }
+                Some(ch) => {
+                    if ch.is_whitespace() {
+                        self.iter.next();
+                        self.current_pos += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
         }
 
-        let token_start = self.current_pos;
+        let token_start;
 
-        while self.iter.peek().unwrap().is_alphanumeric() {
-            self.iter.next();
-            self.current_pos += 1;
+        match self.iter.peek() {
+            token if token.unwrap().is_alphabetic() => {
+                token_start = self.collect_identifier();
+            }
+            Some(&'_') => {
+                token_start = self.collect_identifier();
+            }
+            Some(_) => {
+                token_start = self.current_pos;
+            }
+            None => {
+                return Option::Some(Spanned {
+                    inner: Token::EOF,
+                    loc: Span::Generated,
+                });
+            }
         }
 
-        if self.current_pos > token_start {
-            let span = Span::Span {
-                source: Source::String,
-                content: &self.input[token_start..self.current_pos + 1],
-                start: token_start,
-                end: self.current_pos,
-            };
-            let token = Spanned {
-                inner: Token::Identifier,
-                loc: span,
-            };
-            return Option::Some(token);
-        }
-
-        Some(Spanned {
+        let span = Span::Span {
+            source: Source::String,
+            content: &self.input,
+            start: token_start,
+            end: self.current_pos,
+        };
+        let token = Spanned {
             inner: Token::Identifier,
-            loc: Span::Generated,
-        })
+            loc: span,
+        };
+        Option::Some(token)
+    }
+}
+
+impl<'a> Lexer<'a> {
+    fn collect_identifier(&mut self) -> usize {
+        // as the function is only called when the next character is matched against
+        // is_alphabetic() or the underscore char '_' we can move the current_pos of the lexer
+        let token_start = self.current_pos;
+        self.iter.next();
+        self.current_pos += 1;
+
+        while let Some(ch) = self.iter.peek() {
+            if ch.is_alphanumeric() || ch == &'_' {
+                self.iter.next();
+                self.current_pos += 1;
+            } else {
+                return token_start;
+            }
+        }
+
+        token_start
     }
 }
