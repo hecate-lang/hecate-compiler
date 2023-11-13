0c1b52cbd7e6ae6f51dce351fd20a3d8a462ad0f
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-11-13 15:17:47 +0100

    created raw AST types

--- hecate_parser/src/types.rs
+++ hecate_parser/src/types.rs
@@ -3,42 +3,42 @@ use std::fmt::{Display, Debug};
 use hecate_util::span::Spanned;
 
 #[derive(Default)]
-pub enum RawType<'a> {
+pub enum GenericType<'a, T: Display + PartialEq> {
     #[default]
     Dummy,
     Never,
-    Generic(&'a str, Vec<Spanned<'a, RawType<'a>>>),
-    Tuple(Vec<Spanned<'a, RawType<'a>>>),
-    Slice(Box<Spanned<'a, RawType<'a>>>),
-    Array(Box<Spanned<'a, RawType<'a>>>, Spanned<'a, usize>),
+    Generic(T, Vec<Spanned<'a, GenericType<'a, T>>>),
+    Tuple(Vec<Spanned<'a, GenericType<'a, T>>>),
+    Slice(Box<Spanned<'a, GenericType<'a, T>>>),
+    Array(Box<Spanned<'a, GenericType<'a, T>>>, Spanned<'a, usize>),
 }
 
-impl<'a> RawType<'a> {
+impl<'a, T: Display + PartialEq> GenericType<'a, T> {
     pub fn is_unit(&self) -> bool {
         match self {
-            RawType::Tuple(v) => v.is_empty(),
+            GenericType::Tuple(v) => v.is_empty(),
             _ => false
         }
     }
 
     pub fn is_never(&self) -> bool {
-        matches!(self, RawType::Never)
+        matches!(self, GenericType::Never)
     }
 
     pub fn is_dummy(&self) -> bool {
-        matches!(self, RawType::Dummy)
+        matches!(self, GenericType::Dummy)
     }
 
     pub fn unit() -> Self {
         Self::Tuple(vec![])
     } 
     
-    pub fn simple(t: &'a str) -> Self {
+    pub fn simple(t: T) -> Self {
         Self::Generic(t, vec![])
     }
 }
 
-impl<'a> PartialEq for RawType<'a> {
+impl<'a, T: Display + PartialEq> PartialEq for GenericType<'a, T> {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (Self::Dummy, _) | (_, Self::Dummy) => true,
@@ -52,29 +52,29 @@ impl<'a> PartialEq for RawType<'a> {
     }
 }
 
-impl<'a> Debug for RawType<'a> {
+impl<'a, T: Display + PartialEq> Debug for GenericType<'a, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self}")
     }
 }
 
-impl<'a> Display for RawType<'a> {
+impl<'a, T: Display + PartialEq> Display for GenericType<'a, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            RawType::Never => write!(f, "!"),
-            RawType::Dummy => write!(f, "_"),
-            RawType::Generic(name, v) => if v.is_empty() { 
+            GenericType::Never => write!(f, "!"),
+            GenericType::Dummy => write!(f, "_"),
+            GenericType::Generic(name, v) => if v.is_empty() { 
                 write!(f, "{}", name) 
             } else { 
                 write!(f, "{}<{}>", name, v.iter().map(|t| t.to_string()).collect::<Vec<_>>().join(", ")) 
             },
-            RawType::Tuple(v) => if v.len() == 1 { 
+            GenericType::Tuple(v) => if v.len() == 1 { 
                 write!(f, "({},)", *v[0]) 
             } else { 
                 write!(f, "({})", v.iter().map(|t| t.to_string()).collect::<Vec<_>>().join(", ")) 
             },
-            RawType::Slice(t) => write!(f, "[{}]", ***t),
-            RawType::Array(t, l) => write!(f, "[{};{}]", ***t, **l) 
+            GenericType::Slice(t) => write!(f, "[{}]", ***t),
+            GenericType::Array(t, l) => write!(f, "[{};{}]", ***t, **l) 
         }
     }
 }
@@ -83,17 +83,17 @@ impl<'a> Display for RawType<'a> {
 mod tests {
     use hecate_util::span::Span;
 
-    use crate::types::RawType;
+    use crate::types::GenericType;
 
     #[test]
     fn type_printing() {
         assert_eq!(
-            RawType::Generic("Boof", vec![
-                Span::dummied(RawType::simple("i32")), 
-                Span::dummied(RawType::Tuple(vec![
-                    Span::dummied(RawType::Dummy),
-                    Span::dummied(RawType::Array(
-                        Box::new(Span::dummied(RawType::simple("i32"))), 
+            GenericType::Generic("Boof", vec![
+                Span::dummied(GenericType::simple("i32")), 
+                Span::dummied(GenericType::Tuple(vec![
+                    Span::dummied(GenericType::Dummy),
+                    Span::dummied(GenericType::Array(
+                        Box::new(Span::dummied(GenericType::simple("i32"))), 
                         Span::dummied(16)
                     ))
                 ]))
@@ -104,33 +104,33 @@ mod tests {
 
     #[test]
     fn special_types() {
-        assert!(RawType::Never.is_never());
-        assert!(RawType::Dummy.is_dummy());
-        assert!(RawType::Tuple(vec![]).is_unit());
-        assert_eq!(RawType::Tuple(vec![]), RawType::unit());
-        assert!(!RawType::Tuple(vec![Span::dummied(RawType::Never)]).is_unit());
+        assert!(GenericType::<&str>::Never.is_never());
+        assert!(GenericType::<&str>::Dummy.is_dummy());
+        assert!(GenericType::<&str>::Tuple(vec![]).is_unit());
+        assert_eq!(GenericType::<&str>::Tuple(vec![]), GenericType::unit());
+        assert!(!GenericType::<&str>::Tuple(vec![Span::dummied(GenericType::Never)]).is_unit());
     }
 
     #[test]
     fn types_comp() {
         assert_eq!(
-            RawType::Dummy,
-            RawType::Generic("Foo", vec![Span::dummied(RawType::simple("i32"))])
+            GenericType::Dummy,
+            GenericType::Generic("Foo", vec![Span::dummied(GenericType::simple("i32"))])
         );
         assert_eq!(
-            RawType::Generic("Boof", vec![
-                Span::dummied(RawType::simple("i32")), 
-                Span::dummied(RawType::Tuple(vec![
-                    Span::dummied(RawType::simple("u1024")),
-                    Span::dummied(RawType::Dummy)
+            GenericType::Generic("Boof", vec![
+                Span::dummied(GenericType::simple("i32")), 
+                Span::dummied(GenericType::Tuple(vec![
+                    Span::dummied(GenericType::simple("u1024")),
+                    Span::dummied(GenericType::Dummy)
                 ]))
             ]),
-            RawType::Generic("Boof", vec![
-                Span::dummied(RawType::simple("i32")), 
-                Span::dummied(RawType::Tuple(vec![
-                    Span::dummied(RawType::Dummy),
-                    Span::dummied(RawType::Array(
-                        Box::new(Span::dummied(RawType::simple("i32"))), 
+            GenericType::Generic("Boof", vec![
+                Span::dummied(GenericType::simple("i32")), 
+                Span::dummied(GenericType::Tuple(vec![
+                    Span::dummied(GenericType::Dummy),
+                    Span::dummied(GenericType::Array(
+                        Box::new(Span::dummied(GenericType::simple("i32"))), 
                         Span::dummied(16)
                     ))
                 ]))
